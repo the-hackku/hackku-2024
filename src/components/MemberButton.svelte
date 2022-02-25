704cca2d1bb0f925e3aba237dce9ec31a423892e
Ethan Behrends <epbehrends@gmail.com>
2022-02-24 23:14:24 -0600

    Updates 2/24

---
+++ src/components/MemberButton.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let name
+    export let data
+
+    const hands = ["peace", "wave"]
+</script>
+
+<style>
+    .hand {
+        transform-origin: 100% 100%;
+        transform: rotate(-90deg);
+        transition: .3s;
+        opacity: 0;
+    }
+    
+    .handCont:hover .hand {
+        transform: rotate(0deg);
+        opacity: 1;
+    }
+</style>
+
+{#if name}
+<div class="flex flex-col gap-4">
+    <div on:click={() => window.open(data.link, "_blank")} class="handCont cursor-pointer relative overflow-hidden w-[250px] h-[250px] border-[#e8000d] border-4 rounded-full">
+        <img class="hand absolute w-[60px] left-[10px] top-[90px]" alt="hand" src={`${hands[Math.floor(Math.random() * hands.length)]}.png`} />
+        <img alt={name} src={`${name}.png`} />
+    </div>
+    <div class="text-center red font-bold text-2xl">{data.display}</div>
+    <div class="text-center red text-lg">{data.title}</div>
+</div>
+{/if}
--- src/components/RegisterButton.svelte
+++ src/components/RegisterButton.svelte
@@ -1,48 +1,32 @@
 <script>
-    export let onClick = null
+    export let onClick = () => {}
 </script>
 
 <style>
-    .cont {
-        position: relative;
-        width: 250px;
-        height: 15px;
-        padding: 2em;
-        box-sizing: border-box;
-        cursor: pointer;
-    }
+.animated {
 
-    .backdrop {
-        background-color: #0051ba;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        z-index: 1;
-        border-radius: 100%;
-        transform: skewY(-5deg);
-        transition: 200ms;
-    }
+}
+.skewed {
+    transform: skew(-35deg) scaleY(.5)
+}
+.star {
+    transform: rotate(-45deg) translateX(118px) rotate(45deg)  scaleY(2) skew(30deg) ;
+    transition: 1111ms;
+}
 
-    .cont:hover .backdrop{
-        transform: skewY(5deg);
-    }
-
-    .text {
-        display: flex;
-        width: 100%;
-        height: 100%;
-        justify-content: center;
-        justify-items: center;
-        position: absolute;
-        z-index: 2;
-        color: #f2f2dd;
-    }
+.animated:hover .star {
+    transform: rotate(-405deg) translateX(118px) rotate(405deg)  scaleY(2) skew(30deg) ;
+}
 </style>
 
-<div class="cont">
-    <div class="backdrop"></div>
-    <br>
-    <div class="text">
-        <slot></slot>
+<div on:click={onClick} class="cursor-pointer italic w-[240px] h-[100px] flex relative justify-center items-center animated">
+    <div class="skewed absolute w-[180px] h-[180px] bg-[#0051ba] py-4 px-8 text-xl rounded-[100%]"></div>
+    <div class="skewed absolute w-[240px] h-[240px] border-[4px] border-[#e8000d] rounded-[100%] flex justify-center items-center">
+        <div class="w-[30px] h-[30px] star">
+            <svg class="h-full" id="fdjhsuiobsdo" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style>.fduhsiaf{fill:#f2f2dd;}.fjdsoiufhdsi{fill:#e8000d;}</style></defs><circle class="fduhsiaf" cx="16" cy="16" r="6.26"/><path class="fjdsoiufhdsi" d="M30,14A12,12,0,0,1,18,2a2,2,0,0,0-4,0A12,12,0,0,1,2,14a2,2,0,0,0,0,4A12,12,0,0,1,14,30a2,2,0,0,0,4,0A12,12,0,0,1,30,18a2,2,0,0,0,0-4ZM9.74,16A16.1,16.1,0,0,0,16,9.74,16.1,16.1,0,0,0,22.26,16,16.1,16.1,0,0,0,16,22.26,16.1,16.1,0,0,0,9.74,16Z"/></svg>
+        </div>
+    </div>
+    <div class="absolute w-[180px] h-[180px] text-[#f2f2dd] flex justify-center items-center">
+        <slot />
     </div>
 </div>
