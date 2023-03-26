f4f15a82363de8ab56e630c801b2821ac987a628
Vegito2367 <tejaaditya.g@gmail.com>
2023-03-25 21:18:30 -0500

    Rearranged the images based on max-heights

--- src/App.svelte
+++ src/App.svelte
@@ -159,6 +159,7 @@
 		--accent2: #92A0CA;
 		--accent3: #BFC6D5;
 		--accent4: #CED4DA;
+		--max-sponsor-image-height:3rem;
 	}
 	/* BODY */
 	main {
@@ -194,12 +195,7 @@
 		font-weight: 700;
 		color: var(--body-text);
 	}
-	h3 {
-		font-family: 'PT Sans';
-		font-size: 1.313   rem;
-		font-weight: 700;
-		color: var(--body-text);
-	}
+	
 	.body-text {
 		font-family: 'PT Sans';
 		font-size: 2rem;
@@ -379,7 +375,7 @@
 	}
 	.sponsor-image {
 		margin-top: 7%;
-		max-width: 40rem;
+		max-width: 25rem;
 	}
 
 	.sponsor-tiers{
@@ -607,51 +603,51 @@
 	
 	<div style="background-color: var(--background); height:10rem; padding-left: 7.5rem;" >
 		<h1>Sponsors</h1>
-		<h3>Interested in sponsoring HackKU23?</h3>
-		<h3>Email us at <a href="mailto:sponsorship.hackku@gmail.com">sponsorship.hackku@gmail.com</a></h3>
+		<p class="body-text" style="font-weight:700">Interested in sponsoring HackKU23?</p>
+		<p class="body-text" style="font-weight:700">Email us at <a href="mailto:sponsorship.hackku@gmail.com">sponsorship.hackku@gmail.com</a></p>
 	</div>
 	<div class="section" id="sponsors" style="row-gap:4rem; align-items:center;">
 		
 		<div class="sponsor-tiers">
 			<h2>PETA Tier</h2>
 			<div class="sponsor-image-holders">
-				<img src="security_benefit.png" alt="Security Benefit Logo" class="sponsor-image">
+				<img src="security_benefit.png" alt="Security Benefit Logo" style="max-height:6rem; margin-top: 7%">
 			</div>
 		</div>
 		<div class="sponsor-tiers">
 			<h2>TERA Tier</h2>
 			<div class="sponsor-image-holders">
-				<img src="Ripple.png" alt="Ripple Logo" class="sponsor-image">
+				<img src="ripple (1).png" alt="Ripple Logo"  style="max-height:6rem; margin-top: 7%">
 			</div>
 		</div>
 		<div class="sponsor-tiers">
 			<h2>GIGA Tier</h2>
 			<div class="sponsor-image-holders">
-				<img src="Air Power Consultants.png" alt="HRBlock Logo" class="sponsor-image" style="max-width:25rem">
-				<img src="HRBlock_Logo_250px.png" alt="HRBlock Logo" class="sponsor-image" style="max-width:25rem">
-				<img src="VeteransUnited-HORIZONTAL-[WHITE]-01 (5).png" alt="HRBlock Logo" class="sponsor-image" style="max-width:25rem">
+				<img src="Air Power Consultants.png" alt="HRBlock Logo" class="sponsor-image" style="max-height:15rem">
+				<img src="HRBlock_Logo_250px.png" alt="HRBlock Logo" class="sponsor-image" style="max-height:15rem">
+				<img src="VUHL-Logo-V-large 1.png" alt="HRBlock Logo" class="sponsor-image" style="max-height:15rem">
 			</div>
 		</div>
 		<div class="sponsor-tiers">
 			<h2>MEGA Tier</h2>
 			<div class="sponsor-image-holders">
-				<img src="Tradebot Logo-flat-Registered Trademark.png" alt="TradeBot Logo" class="sponsor-image">
+				<img src="Tradebot Logo-flat-Registered Trademark.png" alt="TradeBot Logo" class="sponsor-image" style="max-height:17rem">
 			</div>
 		</div>
 		<div class="sponsor-tiers">
 			<h2>FOOD</h2>
 			<div class="sponsor-image-holders">
-				<img src="laestrella (1).png" alt="Laestralla Logo" class="sponsor-image" style="max-width:25rem">
-				<img src="mcclains.png" alt="Mcclains Logo" class="sponsor-image" style="max-width:25rem">
-				<img src="globeindianfood.png" alt="Globe Indian Logo" class="sponsor-image" style="max-width:25rem">
+				<img src="laestrella (1).png" alt="Laestralla Logo" class="sponsor-image" style="max-height:17rem">
+				<img src="mcclains.png" alt="Mcclains Logo" class="sponsor-image" style="max-height:17rem">
+				<img src="globeindianfood.png" alt="Globe Indian Logo" class="sponsor-image" style="max-height:17rem">
 			</div>
 		</div>
 		<div class="sponsor-tiers">
 			<h2>IN KIND</h2>
 			<div class="sponsor-image-holders">
-				<img src="GoogleCloud_FullColor 2900x512px.png" alt="Google Cloud Logo" class="sponsor-image" style="max-width:25rem">
-				<img src="echo3D - Logo 2021 - Dark.png" alt="Echo 3D Logo" class="sponsor-image" style="max-width:25rem">
-				<img src="balsamiq-logo-screen.png" alt="Balsamiq Logo" class="sponsor-image" style="max-width:25rem">
+				<img src="GoogleCloud_FullColor 2900x512px.png" alt="Google Cloud Logo" class="sponsor-image" style="max-height:8rem">
+				<img src="echo3D - Logo 2021 - Dark.png" alt="Echo 3D Logo" class="sponsor-image" style="max-height:8rem">
+				<img src="balsamiq-logo-screen.png" alt="Balsamiq Logo" class="sponsor-image" style="max-height:8rem">
 			</div>
 		</div>
 		<div style="display:flex;justify-content:center;align-items:center">
