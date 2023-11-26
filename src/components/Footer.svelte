0bfef33a8fddaafe8abdc16d3ce167877dd8993f
zoekulphong <zckulphong@gmail.com>
2023-11-26 01:06:53 -0600

    upload

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,39 +1,54 @@
+<script>
+	import '../global.css';
+	import Heart from './Heart.svelte'
+  import Scallop from './assets/scallop.svelte'
+</script>
+
+<footer>
+  <div class="footer-container">
+    <div class="heart-container">
+      <Heart />
+      <p class="heart-message">Made with love by the HackKU24 team</p>
+    </div>
+    <div class="links-container">
+      <a href="https://www.instagram.com/thehackku/" target="_blank"><img src="images/instagram.png" class="contact-icon shrink" alt="Instagram icon" /></a>
+      <a href="mailto: hack@ku.edu"><img src="images/email.png" class="contact-icon shrink" alt="Email icon" /></a>
+      <a href="https://github.com/theHackKU" target="_blank"><img src="images/github.png" class="contact-icon shrink" alt="GitHub icon" /></a>
+    </div>
+    <Scallop />
+  </div> 
+</footer>
 
 <style>
-  footer {
+  .footer-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    z-index: 1000;
+    background-color: var(--dark-green);
+  }
+  .heart-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;    
+    gap: 0.75rem;
+  }
+  .heart-message {
+    color: var(--off-white);
+    margin: 0;
+  }
+  .links-container {
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 1rem;
-    background-color: #7085C1;
-    width: fill;
-    height: 4rem;
-    margin: 0 auto;
+    gap: 2.5rem;
+    margin: 1.5rem 0;
   }
-  footer img {
-    width: 3rem;
+  .contact-icon {
     height: 3rem;
-    margin: 0 1rem;
-    object-fit: contain;
   }
 </style>
-
-<footer>
-  <div>
-    <a href="https://www.instagram.com/thehackku/" target="_blank">
-      <img src="ig-logo.png" alt="Instagram icon" />
-    </a>
-  </div>
-  <div>
-    <a href="https://github.com/theHackKU" target="_blank">
-      <img src="github.png" alt="GitHub icon" />
-    </a>
-  </div>
-  <div>
-    <a href="mailto: info.hackku@gmail.com">
-      <img src="email-logo.png" alt="Email icon" />
-    </a>
-  </div>
-</footer>
 
   
