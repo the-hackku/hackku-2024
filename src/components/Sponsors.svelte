0039b5911a0d704b92042cafdacd12e1d9374775
zoekulphong <zckulphong@gmail.com>
2024-03-16 15:37:32 -0500

    add sponsor logos

--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -12,39 +12,43 @@
       </duv>
       <p style="margin-top:0;text-align:center">Interested in sponsoring HackKU24?<br>Contact us at <a href="mailto:hack@ku.edu" class="email-link turn-red">hack@ku.edu</a></p>
     </div>
-    <!-- <h3 id="sponsors-divider">Presented by</h3>
+
     <div class="sponsors-tier">
-      <img src="images/20.png" alt="sponsor1">
+      <h3>PETA Tier</h3>
+      <div class="sponsors-logos">
+        <img src="images/SecurityBenefit.png" alt="Security Benefit">
+      </div>
     </div>
-    <h3 id="sponsors-divider">PETA Tier</h3>
+
     <div class="sponsors-tier">
-      <img src="images/20.png" alt="sponsor2">
-      <img src="images/20.png" alt="sponsor3">
-      <img src="images/20.png" alt="sponsor4">
+      <h3>GIGA Tier</h3>
+      <div class="sponsors-logos">
+        <img src="images/AirPower.png" alt="Air Power">
+        <img src="images/HRBlock.png" alt="HR Block">
+        <img src="images/AdAstra.png" alt="Ad Astra">
+      </div>
     </div>
-    <h3 id="sponsors-divider">TERA Tier</h3>
-    <div class="sponsors-tier" alt="sponsor5"> 
-      <img src="images/20.png" alt="sponsor6">
-      <img src="images/20.png" alt="sponsor7">
-      <img src="images/20.png" alt="sponsor8">
+
+    <div class="sponsors-tier"> 
+      <h3>MEGA Tier</h3> 
+      <div class="sponsors-logos">
+        <img src="images/Netsmart.png" alt="Netsmart">
+        <img src="images/BlackVeatch.png" alt="Black & Veatch">
+      </div>
     </div>
-    <h3 id="sponsors-divider">GIGA Tier</h3>
-    <div class="sponsors-tier" alt="sponsor9">
-      <img src="images/20.png" alt="sponsor10">
-      <img src="images/20.png" alt="sponsor11">
-      <img src="images/20.png" alt="sponsor12">
-      <img src="images/20.png" alt="sponsor13">
+    
+    <div class="sponsors-tier"> 
+      <h3>KILA Tier</h3> 
+      <div class="sponsors-logos">
+        <img src="images/Tradebot.png" alt="Tradebot">
+      </div>
     </div>
-    <h3 id="sponsors-divider">MEGA Tier</h3>
+    
     <div class="sponsors-tier">  
-      <img src="images/20.png" alt="sponsor14">
-      <img src="images/20.png" alt="sponsor15">
-      <img src="images/20.png" alt="sponsor16">
-      <img src="images/20.png" alt="sponsor17">
-    </div> -->
-    <h3 id="sponsors-divider">In-Kind</h3>
-    <div class="sponsors-tier">  
-      <a href="http://hackp.ac/mlh-StandOutStickers-hackathons" target="_blank"><img src="images/standoutStickers.png" alt="Standout Stickers Logo" class="sponsor-logo"></a>
+      <h3>In-Kind</h3>
+      <div class="sponsors-logos">
+        <a href="http://hackp.ac/mlh-StandOutStickers-hackathons" target="_blank"><img src="images/standoutStickers.png" alt="Standout Stickers Logo" class="sponsor-logo"></a>
+      </div>
     </div>
   </div>
 </div>
@@ -56,21 +60,21 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
-    padding-top: 0;
   }
-  /* #sponsors-divider {
-    text-align: center;
-    margin-top: 100px;
-    margin-bottom: 30px;
-  } */
   .sponsors-tier {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content:center;
+    margin-bottom: 6rem;
+  }
+  .sponsors-logos {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap; 
     justify-content:center;
-    row-gap: 10px;
-    column-gap: 10px;
-    margin: 20px;
+    row-gap: 4rem;
+    column-gap: 4rem;
   }
   .flower-decor-container {
     display: flex;
@@ -85,7 +89,8 @@
     color:var(--blue-dk);
     text-decoration:none
   }
-  .sponsor-logo {
-    height: 20rem;
+  img {
+    height: 15vh;
+    max-width: 80vw;
   }
 </style>
